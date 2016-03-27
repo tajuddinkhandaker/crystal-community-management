5fb6f659ddcd4512a9ae588938ccca6acebc70f0
Tajuddin Khandaker <tajuddin.khandaker.cse.ju@gmail.com>
2016-03-27 13:29:46 +0600

    [reference error][jquery package added + polymer element reference fixed]

--- resources/views/master-main.blade.php
+++ resources/views/master-main.blade.php
@@ -15,6 +15,8 @@
         <link rel="stylesheet" href="css/main.css">
         @yield('header-styles')
 
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
         <!-- sweet alert -->
         <script src="bower_components/sweetalert/dist/sweetalert.min.js"></script>
         <link rel="stylesheet" type="text/css" href="bower_components/sweetalert/dist/sweetalert.css">
@@ -24,7 +26,7 @@
         <link rel="import" href="bower_components/paper-material/paper-material.html">
         <link rel="import" href="bower_components/paper-drawer-panel/paper-drawer-panel.html">
         <link rel="import" href="bower_components/paper-header-panel/paper-header-panel.html">
-        <link rel="import" href="bower_componentspaper-scroll-header-panel/paper-scroll-header-panel.html">
+        <link rel="import" href="bower_components/paper-scroll-header-panel/paper-scroll-header-panel.html">
         <link rel="import" href="crystal-elements/crystal-header-menubar.html">
         <link rel="import" href="crystal-elements/crystal-social-fab.html">
         <link rel="import" href="crystal-elements/crystal-drawer-item.html">
@@ -85,9 +87,6 @@
         </paper-drawer-panel>
 
     </body>
-
-    <script src="jquery/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
     <script>
 
         function disposeAlert() {
